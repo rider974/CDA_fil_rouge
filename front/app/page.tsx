df000bbc11a02e319ef4e952e91acbd1b6393493
Thomas Legrand <pro.legrand.thomas@gmail.com>
2024-08-12 02:55:06 +0200

    Front: / add roles

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -100,53 +100,60 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <h2 className="mb-3 text-2xl font-semibold">Users List</h2>
-        <table className="w-full border-collapse border border-gray-200">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 p-2">UUID</th>
-              <th className="border border-gray-300 p-2">Username</th>
-              <th className="border border-gray-300 p-2">Email</th>
-              <th className="border border-gray-300 p-2">Role</th>
-              <th className="border border-gray-300 p-2">Active</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user.user_uuid}>
-                <td className="border border-gray-300 p-2">{user.user_uuid}</td>
-                <td className="border border-gray-300 p-2">{user.username}</td>
-                <td className="border border-gray-300 p-2">{user.email}</td>
-                <td className="border border-gray-300 p-2">
-                  {user.role ? user.role.role_name : "No Role"}
-                </td>
-                <td className="border border-gray-300 p-2">
-                  {user.is_active ? "Yes" : "No"}
-                </td>
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:text-left">
+        <div className="mb-6">
+          <h2 className="mb-3 text-2xl font-semibold">Users List</h2>
+          <table className="w-full border-collapse border border-gray-200">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 p-2">UUID</th>
+                <th className="border border-gray-300 p-2">Username</th>
+                <th className="border border-gray-300 p-2">Email</th>
+                <th className="border border-gray-300 p-2">Role</th>
+                <th className="border border-gray-300 p-2">Active</th>
+                <th className="border border-gray-300 p-2">Date de création</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.user_uuid}>
+                  <td className="border border-gray-300 p-2">{user.user_uuid}</td>
+                  <td className="border border-gray-300 p-2">{user.username}</td>
+                  <td className="border border-gray-300 p-2">{user.email}</td>
+                  <td className="border border-gray-300 p-2">
+                    {user.role ? user.role.role_name : "No Role"}
+                  </td>
+                  <td className="border border-gray-300 p-2">
+                    {user.is_active ? "Yes" : "No"}
+                  </td>
+                  <td className="border border-gray-300 p-2">
+                  {new Date(user.created_at).toLocaleDateString()}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
 
-        {/* Tableau des rôles */}
-        <h2 className="mb-3 mt-6 text-2xl font-semibold">Roles List</h2>
-        <table className="w-full border-collapse border border-gray-200">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 p-2">UUID</th>
-              <th className="border border-gray-300 p-2">Role Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {roles.map((role) => (
-              <tr key={role.role_uuid}>
-                <td className="border border-gray-300 p-2">{role.role_uuid}</td>
-                <td className="border border-gray-300 p-2">{role.role_name}</td>
+        <div>
+          <h2 className="mb-3 mt-6 text-2xl font-semibold">Roles List</h2>
+          <table className="w-full border-collapse border border-gray-200">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 p-2">UUID</th>
+                <th className="border border-gray-300 p-2">Role Name</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {roles.map((role) => (
+                <tr key={role.role_uuid}>
+                  <td className="border border-gray-300 p-2">{role.role_uuid}</td>
+                  <td className="border border-gray-300 p-2">{role.role_name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </main>
   );
