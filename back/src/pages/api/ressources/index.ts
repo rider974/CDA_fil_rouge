76e7169161cbe4c7aaef83b175d629e9bf846edf
Thomas Legrand <pro.legrand.thomas@gmail.com>
2024-08-13 12:08:05 +0200

    add ressource road/controller/services

---
+++ back/src/pages/api/ressources/index.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { RessourceService } from '@/services/ressourceService';
+import { RessourceController } from '@/controllers/ressourcesController';
+import { initializeDataSource } from '../../../data-source';
+import Cors from 'nextjs-cors';
+
+const ressourceService = new RessourceService();
+const ressourceController = new RessourceController(ressourceService);
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await initializeDataSource();
+  await Cors(req, res, {
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: 'http://localhost:3000'
+  });
+
+  switch (req.method) {
+    case 'GET':
+      // Check if the request is for a single ressource by ID or all ressources
+      if (req.query.ressource_uuid) {
+        return ressourceController.getRessourceById(req, res);
+      } else {
+        return ressourceController.getAllRessources(req, res);
+      }
+    case 'POST':
+      return ressourceController.createRessource(req, res);
+    case 'PUT':
+      return ressourceController.replaceRessource(req, res);
+    case 'DELETE':
+      return ressourceController.deleteRessource(req, res);
+    default:
+      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
