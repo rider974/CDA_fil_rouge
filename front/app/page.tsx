5cc0cb91551bb5a55ae0cfd1efe8b03a8b3e27a5
Florence Martin <florence.martin469@gmail.com>
2024-08-14 15:55:36 +0200

    feat: add title

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -59,12 +59,16 @@ export default function HomePage() {
     fetchRoles();
   }, []);
 
-
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="mt-10 mb-4 text-4xl font-bold text-white">
+          Welcome to BeginnersAppDev
+        </h1>
+      </div>
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:text-left">
         <div className="mb-6">
           <h2 className="mb-3 text-2xl font-semibold">Users List</h2>
