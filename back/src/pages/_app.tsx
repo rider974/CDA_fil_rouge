ce2532040dfac34d3211e2c0fca2e5f0a8194b5f
rider974 <minatchy.pascal@protonmail.com>
2024-09-06 17:18:16 +0400

    ci(fixing pages api): deploy on VPS

--- back/src/pages/_app.tsx
+++ back/src/pages/_app.tsx
@@ -7,23 +7,20 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Helmet>
+        <Head>
         <meta
           httpEquiv="Content-Security-Policy"
           content="default-src 'self'; script-src 'self'; object-src 'none'; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;"
         />
-
-      <Head>
-        <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; object-src 'none'; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;" />
-
-        <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
-        <meta httpEquiv="X-Frame-Options" content="DENY" />
-        <meta name="X-XSS-Protection" content="1; mode=block" />
-        <meta name="Referrer-Policy" content="no-referrer" />
-
+          <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
+          <meta httpEquiv="X-Frame-Options" content="DENY" />
+          <meta name="X-XSS-Protection" content="1; mode=block" />
+          <meta name="Referrer-Policy" content="no-referrer" />
+          <title>BeginnersAppDev</title>
+        </Head>
       </Helmet>
 
-        <title>BeginnersAppDev</title>
-      </Head>
+ 
 
       <Component {...pageProps} />;
     </>
